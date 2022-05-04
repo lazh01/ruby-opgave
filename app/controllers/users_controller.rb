2fca6a80b6f32e24e3f8fd948a3cf8874c18c342
lazh01 <seb0465@hotmail.com>
2022-05-04 23:48:10 +0200

    added email to user, and cleaned up login visuals.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to root_path
+            redirect_to rooms_index_path
         else
             render :new
         end
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password, :avatar)
+        params.require(:user).permit(:username, :password, :avatar, :email)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates_uniqueness_of :username
+    validates_uniqueness_of :email
     has_secure_password
     has_many :messages
     has_one_attached :avatar do |attachable|
